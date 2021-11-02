28d5e8e6ba28f8453c4a3dd6a380bfb46104ee8d
Corey Crawford <77764602+crawfordc54@users.noreply.github.com>
2021-11-02 16:52:47 -0400

    Add files via upload

---
+++ Lab4/LotteryTicket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Lab4
+{
+    class LotteryTicket
+    {
+        public Collection<int> whiteBalls = new Collection<int>();
+        public int redBall;
+        public LotteryTicket()
+        {
+            Random rand = new Random();
+            whiteBalls.Add(rand.Next(1, 69));
+            whiteBalls.Add(rand.Next(1, 69));
+            whiteBalls.Add(rand.Next(1, 69));
+            whiteBalls.Add(rand.Next(1, 69));
+            whiteBalls.Add(rand.Next(1, 69));
+
+            redBall = (rand.Next(1, 26));
+        }
+        public Collection<int> Draw()
+        {
+            Random rand = new Random();
+            Collection<int> winningNumbers = new Collection<int>();
+            winningNumbers.Add(rand.Next(1, 69));
+            winningNumbers.Add(rand.Next(1, 69));
+            winningNumbers.Add(rand.Next(1, 69));
+            winningNumbers.Add(rand.Next(1, 69));
+            winningNumbers.Add(rand.Next(1, 69));
+            winningNumbers.Add(rand.Next(1, 26));
+            return winningNumbers;
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Lab4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			LotteryTicket ticket = new LotteryTicket();
+			int numOfChecks = 0;
+			Boolean matched = false;
+			Collection <int> winningNumbers;
+			while(!matched)
+            {
+				winningNumbers = ticket.Draw();
+				numOfChecks++;
+				if (ticket.whiteBalls[0] == winningNumbers[0] &&
+				   ticket.whiteBalls[1] == winningNumbers[1] &&
+				   ticket.whiteBalls[2] == winningNumbers[2] &&
+				   ticket.whiteBalls[3] == winningNumbers[3] &&
+				   ticket.whiteBalls[4] == winningNumbers[4] &&
+				   ticket.redBall == winningNumbers[5])
+                {
+					matched = true;
+                }
+			}
+			Console.WriteLine("White Ball Numbers: " + ticket.whiteBalls[0] + " " + ticket.whiteBalls[1] + " " + ticket.whiteBalls[2] + " " + ticket.whiteBalls[3] + " " + ticket.whiteBalls[4]);
+			Console.WriteLine("Red Ball Number: " + ticket.redBall);
+			Console.WriteLine("Number of Tries to Win: " + numOfChecks);
+		}
+	}
+}
+
